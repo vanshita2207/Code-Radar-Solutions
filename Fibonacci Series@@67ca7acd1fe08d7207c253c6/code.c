14ce6ff81f8bc84d71d05e72b613f84ed5646493
Vanshita Taneja <vanshita.taneja22@gmail.com>
2025-03-30 01:00:48 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,7 +2,7 @@ void fibonacciSeries(int n) {
     int fib[n]; 
     fib[0] = 0, fib[1] = 1;
 
-    // printf("%d %d ", fib[0], fib[1]);
+    printf("%d %d ", fib[0], fib[1]);
 
     for (int i = 2; i < n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
