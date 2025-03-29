67993bc73032bb9ff0286d5b1de4b932bbe24d2d
Vanshita Taneja <vanshita.taneja22@gmail.com>
2025-03-30 01:10:14 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Comparison function for sorting
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
 
 // Function to find k-th smallest element
 int kthSmallest(int arr[], int n, int k) {
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k\n");
+        return -1; // Error handling
+    }
+
     qsort(arr, n, sizeof(int), compare);  // Sort array
     return arr[k - 1];  // Return k-th smallest element
 }
