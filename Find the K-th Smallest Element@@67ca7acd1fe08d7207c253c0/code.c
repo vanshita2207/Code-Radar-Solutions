333286d9f0075a61b7d0be9840e2fb0703623c5c
Vanshita Taneja <vanshita.taneja22@gmail.com>
2025-03-30 01:07:25 +0530

    Solution to Find the K-th Smallest Element

---
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -0,0 +1,9 @@
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+// Function to find k-th smallest element
+int kthSmallest(int arr[], int n, int k) {
+    qsort(arr, n, sizeof(int), compare);  // Sort array
+    return arr[k - 1];  // Return k-th smallest element
+}
