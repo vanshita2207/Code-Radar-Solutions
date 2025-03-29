cd4e21305bcec8dd3b5381b84d7319ad4d9b7140
Vanshita Taneja <vanshita.taneja22@gmail.com>
2025-03-30 00:51:04 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -8,6 +8,6 @@ int fibonacciSeries(int n){
     if(n == 1 || n==2){
         return n-1;
     }
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 }
  
