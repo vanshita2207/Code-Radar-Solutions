8836cab06eabd86dc89116c10b4f88a20a489fad
Vanshita Taneja <vanshita.taneja22@gmail.com>
2025-04-11 19:42:24 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,28 +1,56 @@
 // Your code here...
+// #include <stdio.h>
+
+// int sort(int arr[],int n){
+//     for(int i=0; i<n-1; i++){
+//         if(arr[i] > arr[n-i-1]){
+//             return 0;
+//         }
+//         return 1;
+//     }
+    
+// }
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     int arr[n];
+
+    
+//     for(int i=0; i<n; i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     if(sort(arr,n)){
+//         printf("Sorted");
+//     }else{
+//         printf("Not Sorted");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int sort(int arr[],int n){
-    for(int i=0; i<n-1; i++){
-        if(arr[i] > arr[n-i-1]){
+int sort(int arr[], int n){
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] > arr[i + 1]){
             return 0;
         }
-        return 1;
     }
-    
+    return 1;
 }
+
 int main(){
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);  // Read number of elements
     int arr[n];
 
-    
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);  // Read the elements
     }
 
-    if(sort(arr,n)){
+    if(sort(arr, n)){
         printf("Sorted");
-    }else{
+    } else {
         printf("Not Sorted");
     }
     return 0;
